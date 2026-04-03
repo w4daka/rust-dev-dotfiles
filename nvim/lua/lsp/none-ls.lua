2bf90b8ece628a951a8eb30c37ed78f9d80fc905
w4daka <w4daka999@gmail.com>
2026-04-04 08:12:49 +0900

    feat(.zshrc)nvm add

--- nvim/lua/lsp/none-ls.lua
+++ nvim/lua/lsp/none-ls.lua
@@ -1,12 +1,12 @@
 return {
-  "nvimtools/none-ls.nvim",
-  optional = false,
-  opts = function(_, opts)
-    local nls = require("null-ls")
-    opts.sources = vim.list_extend(opts.sources or {}, {
-      nls.builtins.diagnostics.markdownlint_cli2.with({
-        method = nls.methods.DIAGNOSTICS,
-      }),
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	optional = false,
+	opts = function(_, opts)
+		local nls = require("null-ls")
+		opts.sources = vim.list_extend(opts.sources or {}, {
+			nls.builtins.diagnostics.markdownlint_cli2.with({
+				method = nls.methods.DIAGNOSTICS,
+			}),
+		})
+	end,
 }
